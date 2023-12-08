9573218a75a341412a2cc0023e402c2d802abb05
emanuelli01 <emanuellilucca@gmail.com>
2023-12-08 16:33:00 -0300

    final

--- apps/bebidas/models.py
+++ apps/bebidas/models.py
@@ -3,4 +3,5 @@ from products.models import Product
 
 # Create your models here.
 class BebidasProduct(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='bebidas_product')
+    product_reference = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='bebidas_product')
+    product_id = models.IntegerField()
